5acd52f7f1660a5d5a36e3d88edbc24d1517585e
codesonzh <codesonzh@gmail.com>
2017-05-10 04:16:02 +0200

    Remove dead code.

--- src/js/background.js
+++
@@ -1,4 +0,0 @@
-console.info("PoloNinja bg script loaded.");
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    chrome.tabs.executeScript(null, {file: "js/content.js"});
-});
